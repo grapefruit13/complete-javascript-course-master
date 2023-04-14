f94e897aa24304d06262a5dda803cee1fdd78d7b
grapefruit13 <liam.heo11@gmail.com>
2023-04-14 14:02:50 +0900

    79. Modal WIndow, 80. Working with CLasses

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,8 +11,8 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
+let secretNumber = Math.trunc(Math.random() * 100) + 1;
+let score = 15;
 let highscore = 0;
 
 const displayMessage = function (message) {
@@ -108,8 +108,8 @@ document.querySelector('.again').addEventListener('click', function () {
 // solution
 
 document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 15;
+  secretNumber = Math.trunc(Math.random() * 100) + 1;
 
   // document.querySelector('.message').textContent = 'Start guessing...';
   displayMessage('Start guessing...');
